528d4d6e05fed0ece14a4f11e71d5d472cb57429
SubhankarMukherjee <connecttosubhankar@gmail.com>
2021-12-10 22:50:56 +0530

    first Commit

---
+++ src/main/java/com/connecttosubh/ProductserviceApplication.java
@@ -0,0 +1,23 @@
+package com.connecttosubh;
+
+import lombok.Builder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ProductserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductserviceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate(RestTemplateBuilder builder)
+    {
+        return builder.basicAuthentication("couponClientapp","9999").build();
+
+    }
+}
